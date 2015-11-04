865658bad5ad66af89ddcf3164373a2562490ffa
Alexander Freisinger <alexander.freisinger@gmail.com>
2015-11-05 00:37:43 +0100

    Tür

---
+++ unity/unity2015/M's Game/Assets/Scripts/DoorAnimationNew.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimationNew : MonoBehaviour {
+
+	 private Animation anima;
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		anima = this.gameObject.GetComponent<Animation> ();
+		anima.Play ("opennew");
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit (Collider other) {
+		anima = this.gameObject.GetComponent<Animation> ();
+		anima.Play ("closenew");
+	}
+}
